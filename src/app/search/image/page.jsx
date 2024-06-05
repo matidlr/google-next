ef586ace5a6871e160083cdc2d17d8371b5772fb
Matias <mati.delarosa1984@gmail.com>
2024-06-05 13:15:02 -0300

    create search page for web and image

---
+++ src/app/search/image/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ImageSearchpage() {
+  return (
+    <div>ImageSearchPage</div>
+  )
+}
---
+++ src/app/search/web/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function WebSearchpage() {
+  return (
+    <div>websearchpage</div>
+  )
+}
