a7632eb244f19db01ebdc7ee3412845c9845bf54
Matias <mati.delarosa1984@gmail.com>
2024-06-05 16:03:03 -0300

    handle possible errors

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
 
-export default function WebSearchpage() {
+export default async function WebSearchPage({searchParams}) {
+  
+  const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}
+  &cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`);
+  const data = await response.json();
+  const results = data.items;
+
+  if (!results) {
+    return (
+      <div className='flex flex-col justify-center items-center pt-10'>
+        <h1 className='text-3xl mb-4'>
+          No results found for {searchParams.searchTerm}
+        </h1>
+        <p className='text-lg'>
+          Try searching the web or images for something else{' '}
+          <Link href='/' className='text-blue-500'>
+            Home
+          </Link>
+        </p>
+      </div>
+    );
+  }
+
   return (
-    <div>websearchpage</div>
+    <div>{results && results.map((result) => <h1>{result}</h1>)}</div>
   )
 }
