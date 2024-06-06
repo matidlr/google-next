b989c81073dd5f76d7c49ec7062d9a0d2dd49e88
Matias <mati.delarosa1984@gmail.com>
2024-06-06 12:16:52 -0300

    fix

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,10 +1,13 @@
-import WebSearchResults from "@/components/WebSearchResults";
-import Link from "next/link";
+import WebSearchResults from '@/components/WebSearchResults';
+import Link from 'next/link';
 
-export default async function WebSearchPage({searchParams}) {
-  
-  const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}
-  &cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`);
+export default async function WebSearchPage({ searchParams }) {
+  const startIndex = searchParams.start || '1';
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}'}&start=${startIndex}`
+  );
+  if (!response.ok) throw new Error('Something went wrong');
   const data = await response.json();
   const results = data.items;
 
@@ -24,6 +27,5 @@ export default async function WebSearchPage({searchParams}) {
     );
   }
 
-  return  <div>{results && <WebSearchResults  results={data}/>}</div>;
-  
+  return <div>{results && <WebSearchResults results={data} />}</div>;
 }
